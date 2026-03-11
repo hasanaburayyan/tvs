83bbc533bbe77645a5aaaa411ef0593a75fd7d73
Hasan <hasanaburayyan21@gmail.com>
2026-03-11 18:50:03 -0400

    fixed targeting

--- godot/tvs-sharp/scenes/hud/hud elements/Targeting.cs
+++ godot/tvs-sharp/scenes/hud/hud elements/Targeting.cs
@@ -33,7 +33,7 @@ public partial class Targeting : Control
     return _camera;
   }
 
-  public override void _Input(InputEvent @event)
+  public override void _UnhandledInput(InputEvent @event)
   {
     if (@event is not InputEventMouseButton mb) return;
     if (mb.ButtonIndex != MouseButton.Left || !mb.Pressed) return;
